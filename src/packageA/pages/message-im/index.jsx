1ae1cbf3b52160c9952d8fc59afdabb03c310474
huangbs <huangboshengustb@163.com>
2020-12-16 23:14:35 +0800

    fix: 修复聊天图片显示变形问题

--- src/packageA/pages/message-im/index.jsx
+++ src/packageA/pages/message-im/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Taro from '@tarojs/taro'
 import { View, Text, Image, Textarea, ScrollView, Video } from '@tarojs/components'
 import PhotoPickerIm from '@components/photo-picker-im'
-import {ICONS} from '@common/constant'
+import { ICONS } from '@common/constant'
 import Presenter from './presenter'
 import './index.scss'
 
@@ -31,83 +31,87 @@ export default class MessageIMView extends Presenter {
             scrollTop={scrollTop}
           >
             <View className='content-wrapper'>
-            {
-              messageList.map(item => {
-                return (
-                  <View className="chat-rotate-wraper">
-                    {
-                      item.uid === userId ?
-                        <View className="chat-box-item-right">
-                          <View className="chat-box-item-right-content">
-                            <View className="nickname">{item.nickName}</View>
-                            {
-                              !item.files.type && item.content && 
-                              <View selectable={true} className="content">{item.content}
+              {
+                messageList.map(item => {
+                  return (
+                    <View className="chat-rotate-wraper">
+                      {
+                        item.uid === userId ?
+                          <View className="chat-box-item-right">
+                            <View className="chat-box-item-right-content">
+                              <View className="nickname">{item.nickName}</View>
                               {
-                                item.isBlock &&
-                                <Image className="block" src={ICONS.BLOCK} onClick={this.blockInfo.bind(this)}></Image>
+                                !item.files.type && item.content &&
+                                <View selectable={true} className="content">{item.content}
+                                  {
+                                    item.isBlock &&
+                                    <Image className="block" src={ICONS.BLOCK} onClick={this.blockInfo.bind(this)}></Image>
+                                  }
+                                </View>
                               }
-                              </View>
-                            }
-                            {
-                              item.files.type == 1 && 
-                              <View style={{marginTop:'20px'}}>
-                                <Image onClick={this.preViewImage.bind(this,item.files.url)} style={{maxWidth:'200px'}} src={item.files.url}></Image>
                               {
-                                item.isBlock &&
-                                <Image className="block" src={ICONS.BLOCK} onClick={this.blockInfo.bind(this)}></Image>
-                              }
-                              </View>
-                            }
+                                item.files.type == 1 &&
+                                <View style={{ marginTop: '20px' }}>
+                                  {
+                                    item.isBlock ?
+                                      <Image className="block" src={ICONS.BLOCK} onClick={this.blockInfo.bind(this)}></Image>
+                                      :
+                                      <View className='image-wrapper' style={{ backgroundImage: `url(${item.files.url})`,backgroundPosition: 'right center' }} onClick={this.preViewImage.bind(this, item.files.url)}>
+                                      </View>
+                                  }
+                                </View>
 
-                            {
-                              item.files.type == 2 && 
-                              <View style={{marginTop:'20px'}}>
-                                <Video style={{maxWidth:'200px'}} src={item.files.url}></Video>
-                              {
-                                item.isBlock &&
-                                <Image className="block" src={ICONS.BLOCK} onClick={this.blockInfo.bind(this)}></Image>
+
                               }
-                              </View>
-                            }
-                            
+
+                              {
+                                item.files.type == 2 &&
+                                <View style={{ marginTop: '20px' }}>
+                                  <Video style={{ maxWidth: '200px' }} src={item.files.url}></Video>
+                                  {
+                                    item.isBlock &&
+                                    <Image className="block" src={ICONS.BLOCK} onClick={this.blockInfo.bind(this)}></Image>
+                                  }
+                                </View>
+                              }
+
+                            </View>
+                            <Image className="chat-box-item-right-img" onClick={this.viewProfileInfo.bind(this, item.uid)} src={item.headImg}></Image>
                           </View>
-                          <Image className="chat-box-item-right-img" onClick={this.viewProfileInfo.bind(this,item.uid)} src={item.headImg}></Image>
-                        </View>
-                        :
-                        <View className="chat-box-item-left">
-                          <Image className="chat-box-item-left-img" onClick={this.viewProfileInfo.bind(this,item.uid)} src={item.headImg}></Image>
-                          <View className="chat-box-item-left-content">
-                            <View className="nickname">{item.nickName}</View>
-                            {
-                              item.content && 
-                              <Text selectable={true} className="content">{item.content}</Text>
-                            }
-                            {
-                              item.files.type == 1 && 
-                              <View className='image-wrapper' style={{ backgroundImage: `url(${item.files.url})`}} onClick={this.preViewImage.bind(this, item.files.url)}>
-                                {/* <Image  style={{maxWidth:'200px'}} src={item.files.url} mode='widthFix'></Image> */}
-                              </View>
-                            }
-                            {
-                              item.files.type == 2 && 
-                              <Video style={{maxWidth:'200px'}} src={item.files.url}></Video>
-                            }
+                          :
+                          <View className="chat-box-item-left">
+                            <Image className="chat-box-item-left-img" onClick={this.viewProfileInfo.bind(this, item.uid)} src={item.headImg}></Image>
+                            <View className="chat-box-item-left-content">
+                              <View className="nickname">{item.nickName}</View>
+                              {
+                                !item.files.type && item.content &&
+                                <Text selectable={true} className="content">{item.content}</Text>
+                              }
+                              {
+                                item.files.type == 1 &&
+                                <View className='image-wrapper' style={{ backgroundImage: `url(${item.files.url})`, marginTop: '20px'  }} onClick={this.preViewImage.bind(this, item.files.url)}>
+                                  {/* <Image  style={{maxWidth:'200px'}} src={item.files.url} mode='widthFix'></Image> */}
+                                </View>
+                              }
+                              {
+                                item.files.type == 2 &&
+                                <Video style={{ maxWidth: '200px' }} src={item.files.url}></Video>
+                              }
+                            </View>
                           </View>
-                        </View>
-                    }
-                  </View>
-                )
-              })
+                      }
+                    </View>
+                  )
+                })
               }
             </View>
           </ScrollView>
         </View>
-        <View className="input-box" style={{bottom:`${inputBoxBottom}px`}}>
+        <View className="input-box" style={{ bottom: `${inputBoxBottom}px` }}>
           <Textarea value={inputValue} className="input-box-textarea" focus={isFocus} fixed placeholder="请输入" showConfirmBar={false} onFocus={this.onFocus.bind(this)} onBlur={this.onBlur.bind(this)} onInput={this.inputMessage.bind(this)} cursorSpacing={128} autoHeight maxlength="-1" adjustPosition={true} holdKeyboard={holdKeyboard} />
           <PhotoPickerIm uploadedTip="发送成功" uploadingTip="发送中" onGetFiles={this.getFiles.bind(this)}></PhotoPickerIm>
           {
-            activeFocus && 
+            activeFocus &&
             <View className="input-box-btn" onClick={this.publishText.bind(this)}>发送</View>
           }
         </View>
