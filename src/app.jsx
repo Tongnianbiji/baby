274f550815bb0128722764744dcef31db846e629
huangbs <huangboshengustb@163.com>
2020-12-24 20:31:23 +0800

    fix: 修复消息卡片字体溢出问题

--- src/app.jsx
+++ src/app.jsx
@@ -15,7 +15,7 @@ import { USER_INFO_KEY_USERID } from '@common/constant'
 import Storage from '@common/localStorage'
 import './app.scss'
 
-const v = '1.1.5'
+const v = '1.1.6'
 
 const request = new BaseRequest()
 const systemInfo = Taro.getSystemInfoSync()
